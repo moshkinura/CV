4706d0023c80d4e4f12c4ae42f25a7d26f8cdcca
Yury Moshkin <bar.u.xoloda@gmail.com>
2024-01-11 23:16:36 +0700

    updated button change language

--- src/Components/Language.tsx
+++ src/Components/Language.tsx
@@ -1,4 +1,4 @@
-import { Container, Dropdown, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Dropdown, Navbar } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 
 import { FontAwesome } from './FontAwesome';
@@ -15,31 +15,29 @@ export const Language = () => {
   };
 
   return (
-    <>
-      <Navbar>
-        <Container>
-          <Navbar.Collapse className="justify-content-end">
-            <Dropdown>
-              <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-                <FontAwesome icon="fa-solid fa-globe" /> {i18n.t('lang')}
-              </Dropdown.Toggle>
+    <Navbar>
+      <Container>
+        <Navbar.Collapse className="justify-content-end">
+          <Dropdown>
+            <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+              <FontAwesome icon="fa-solid fa-globe" /> {i18n.t('lang')}
+            </Dropdown.Toggle>
 
-              <Dropdown.Menu>
-                {languages.map((item: any, i: number) => {
-                  return (
-                    <Dropdown.Item
-                      onClick={() => changeLanguage(item.code)}
-                      key={i}
-                    >
-                      {item.name}
-                    </Dropdown.Item>
-                  );
-                })}
-              </Dropdown.Menu>
-            </Dropdown>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+            <Dropdown.Menu>
+              {languages.map((item: any, i: number) => {
+                return (
+                  <Dropdown.Item
+                    onClick={() => changeLanguage(item.code)}
+                    key={i}
+                  >
+                    {item.name}
+                  </Dropdown.Item>
+                );
+              })}
+            </Dropdown.Menu>
+          </Dropdown>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
